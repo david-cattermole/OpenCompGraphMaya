a6496f0dd2bc380770de04df787af99fae645c18
David Cattermole <cattermole91@gmail.com>
2021-09-13 00:51:33 +0100

    Display all OCG node info messages to the Output Window or Terminal.

--- src/plugin_main.cpp
+++ src/plugin_main.cpp
@@ -115,7 +115,7 @@ MStatus initializePlugin(MObject obj) {
     ocg::log::initialize();
     ocgm::log::initialize();
     // TODO: Parse environment variables and pass the log level.
-    ocgm::log::set_level("warn");
+    ocgm::log::set_level("info");
     auto log = ocgm::log::get_logger();
     log->info("Initializing OpenCompGraphMaya plug-in...");
 
